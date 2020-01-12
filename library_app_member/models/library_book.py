833482dca91979effd72a8b2b34dc0ec3955b7ac
Cadara <carlosdanielrt@gmail.com>
2020-01-11 20:52:58 -0400

    Modifying existing fields(110) - isbn field on library.book modified to add help attribute - publisher_id field on library.book modified to add index so searches on this field are more efficient

--- library_app_member/models/library_book.py
+++ library_app_member/models/library_book.py
@@ -8,3 +8,11 @@ class Book(models.Model):
 
     is_available = fields.Boolean(string='Is Available?')
 
+    #
+    # all atributes for these fields which were not explicitly mentioned remain unchanged
+    #
+    # adding text to the help dialog when the mouse hovers
+    isbn = fields.Char(help='Use a valid ISBN-13 or ISBN-10.')
+    # indexing the publisher_id field so searches are more efficient
+    publisher_id = fields.Many2one(index=True)
+

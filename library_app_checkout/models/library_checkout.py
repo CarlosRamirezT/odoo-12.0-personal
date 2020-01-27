cafc4b666a2fab5218d9b5e9df9be2c1925620ce
Cadara <carlosdanielrt@gmail.com>
2020-01-27 09:51:09 -0400

    commit starting module 8, odoo development essentials

--- library_app_checkout/models/library_checkout.py
+++ library_app_checkout/models/library_checkout.py
@@ -3,3 +3,25 @@ from odoo import api, exceptions, fields, models
 class Checkout(models.Model):
     _name = 'library.checkout'
     _description = 'Checkout Request'
+
+    member_id = fields.Many2one(
+        comodel_name='library.member',
+        required=True,
+    )
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Librarian',
+        default=lambda s: s.env.uid,
+    )
+    request_date = fields.Date(default=lambda d: fields.Date.today())
+    line_ids = fields.One2many(
+        'library.checkout.line',
+        'checkout_id',
+        string='Borrowed Books',
+    )
+
+class CheckoutLine(models.Model):
+    _name = 'library.checkout.line'
+    _description = 'Borrow Request Line'
+    checkout_id = fields.Many2one(comodel_name='library.checkout')
+    book_id = fields.Many2one(comodel_name='library.book')
